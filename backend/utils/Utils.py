264002a900283c50e43881ad9eec47b57b24091c
andrew <andrewclchan211@vt.edu>
2022-02-26 13:11:04 -0500

    init

--- backend/utils/Utils.py
+++ backend/utils/Utils.py
@@ -1,5 +1,9 @@
 import requests
 import deso
+import hashlib
+import binascii
+import base58
+
 API_URL = "https://node.deso.org/api/v1"
 API_HEADER = {"Content-Type": "application/json; charset=utf-8"}
 
@@ -22,7 +26,7 @@ class Utils:
         prof = deso.Users.getSingleProfile(pubKey)
         #print(prof['Profile'])
         return deso.Users.getTransactionInfo(publicKey=pubKey)
-        
+
     # Gets the most recent block (useful for getting height of most recent)
     @staticmethod
     def queryMostRecentBlock():
@@ -34,4 +38,48 @@ class Utils:
         if k in m:
             return m[k]
         return None
+
+    # Encryption stuff -------------------------------------------------------
+    @staticmethod
+    def doubleSha256(hex):
+        bin = binascii.unhexlify(hex)
+        hash = hashlib.sha256(bin).digest()
+        hash2 = hashlib.sha256(hash).digest()
+        return str(binascii.hexlify(hash2))
+
+    @staticmethod
+    def keyTo58Sum(key, prefix):
+        ascii = prefix+key
+        hex = binascii.hexlify(bytes(ascii,"utf-8"))
+        dSha256 = Utils.doubleSha256(hex)[:4]
+
+        tmp = bytearray()
+        tmp.extend(bytearray(prefix.encode('utf-8')))
+        tmp.extend(bytearray(key.encode('utf-8')))
+        tmp.extend(bytearray(dSha256.encode("utf-8")))
+        return base58.b58encode(tmp)
+
+'''
+        tmp = bytearray()
+        tmp.extend(bytearray(prefix.encode('utf-8')))
+        tmp.extend(bytearray(key.encode('utf-8')))
+        tmp.extend(bytearray(hash2))
+        return base58.b58encode(tmp)
+        '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ backend/utils/testUtils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from Utils import Utils
+
+
+prefix="BC1"
+key="A6j3tHEvrQrpp39XyTqZ47P/1OxQ8vZBLeeIvODSA/YX"
+
+print(Utils.keyTo58Sum(key, prefix))
+
+
+
+
+
+
+
