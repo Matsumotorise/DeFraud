362a9994d36f29b1d7e327c6b370fba32e05b6f6
andrew <andrewclchan211@vt.edu>
2022-02-25 23:57:34 -0500

    Queries up to 5 blocks working and gathering of publicKeys associated

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,51 @@
 #!/usr/bin/env python
 from utils.Utils import Utils
 
-print(Utils.queryMostRecentBlock())
+
+#print(Utils.queryMostRecentBlock())
+
+def main():
+    # Make a call to the Dezos chain and retrieves N most recent blocks
+    maxHeight = Utils.queryMostRecentBlock().json()['Header']['Height']
+
+    # End up with a list of Publickey : [eventType1, eventType2, ...]
+    userHashes = {}
+    n=5
+
+    # Get each block and parse info here
+    for i in range(maxHeight-n, maxHeight):
+        try:
+            data = Utils.queryBlock(i).json()
+        except:
+            print(f"WARN: something went wrong trying to query block {i}")
+            continue
+        # Extract public hashes out of blocks
+        transactions = Utils.safeMapAccess(data, 'Transactions')
+        if transactions == None:
+            print(f"WARN: something went wrong trying to query block {i} @Transactions")
+            continue
+        for t in transactions:
+            metaData = Utils.safeMapAccess(t, 'TransactionMetadata')
+            if(metaData == None):
+                print(f"WARN: something went wrong trying to query block {i} @Metadata")
+                continue
+            affectedKeys = Utils.safeMapAccess(metaData, 'AffectedPublicKeys')
+            if(affectedKeys == None):
+                print(f"WARN: something went wrong trying to query block {i} @AffectedKeys")
+                continue
+            for ak in affectedKeys:
+                curK = ak['PublicKeyBase58Check']
+                if curK not in userHashes:
+                    userHashes[curK] = [ak['Metadata']]
+                else:
+                    userHashes[curK].append(ak['Metadata'])
+
+    print(userHashes)
 
 
 
 
+if __name__ == "__main__":
+    main()
 
 
--- backend/utils/Utils.py
+++ backend/utils/Utils.py
@@ -1,28 +1,30 @@
 import requests
 
 API_URL = "https://node.deso.org/api/v1"
-API_HEADER = {"Content-Type: application/json"}
+API_HEADER = {"Content-Type": "application/json; charset=utf-8"}
+
 
 class Utils:
-    # Makes a call to the Dezos chain and retrieves N most recent blocks
-    @staticmethod
-    def queryLastNBlocks(n):
-        pass
-
     # Post request to get block at height h
     @staticmethod
     def queryBlock(h):
-        payload = {"Height": h, "FullBlock": True}
-        return requests.post(f"{API_URL}/block", data=payload)
+        payload = {"Height": h, "FullBlock": True,}
+        return requests.post(f"{API_URL}/block", headers=API_HEADER, json=payload)
 
     # Gets user's transactions
     @staticmethod
     def queryUser(pubKey):
-        payload = {"PublicKeyBase58Check":pubKey}
-        return requests.post(f"{API_URL}/transaction-info", data=payload)
+        payload = {"PublicKeyBase58Check" : pubKey,}
+        return requests.post(f"{API_URL}/transaction-info", headers=API_HEADER, json=payload)
 
     # Gets the most recent block (useful for getting height of most recent)
     @staticmethod
     def queryMostRecentBlock():
         return requests.get(API_URL)
+
+    @staticmethod
+    def safeMapAccess(m, k):
+        if k in m:
+            return m[k]
+        return None
 
